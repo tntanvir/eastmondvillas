a619d9dfbfaa9128f563e29c8c4fb8cf55e3c676
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-11-05 06:26:03 +0600

    Add JSON structure for property details in views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
 
 # Create your views here.
+ 
+"""
+
+{
+  "title": "Nulla occaecat optio",
+  "description": "Aspernatur possimus",
+  "price": "371",
+  "propertyType": "condo",
+  "guest": "74",
+  "address": "Enim voluptatibus hi",
+  "amenities_indoor": ["Sed rerum ab ea iste"],
+  "amenities_outdoor": ["Ea aut tempor anim i"],
+  "bathrooms": "78",
+  "bedroom_images": [],
+  "bedrooms": "49",
+  "booking_rate_start": "Autem beatae aut dol",
+  "calendarLink": "https://www.gidynu.mobi",
+  "check_in": "Quia nihil officia e",
+  "check_out": "Voluptatem ut except",
+  "city": "Eu optio id magna ",
+  "rules": ['dkjfkj','ksdjfkja'],
+  "latitude": "A laborum veniam qu",
+  "longitude": "Autem enim ex earum ",
+  "media_images": [],
+  "pool": "31",
+  "seo_description": "Ab incidunt ut dolo",
+  "seo_titile": "Consequatur laborum",
+  "signature_distinctions": ["Voluptatem Neque ar"],
+  "staff": "Aut placeat digniss",
+  "status": "Draft",
+  "villa_name": "Melinda York",
+  "location": {
+    "address": "equa",
+    "description": "equa tower ",
+    "latitude": 23.7807517,
+    "longitude": 90.4076056
+  }
+}
+     
+"""
