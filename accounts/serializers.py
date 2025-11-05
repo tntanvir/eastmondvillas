ecfdb14dd31941de972655d687a509ac34a71202
roniahamed <111082653+roniahamed@users.noreply.github.com>
2025-11-05 06:25:59 +0600

    Add custom user serializers for registration and details management

---
+++ accounts/serializers.py
@@ -0,0 +1,42 @@
+from datetime import date 
+from dj_rest_auth.serializers import UserDetailsSerializer 
+from dj_rest_auth.registration.serializers import RegisterSerializer 
+
+from rest_framework import serializers
+from rest_framework.validators  import UniqueValidator 
+
+from .models import User 
+
+class CustomUserDetailsSerializer(UserDetailsSerializer):
+    class meta(UserDetailsSerializer.Meta):
+        model = User 
+        fields = ('id', 'email', 'name','role', 'permission', 'is_verified', 'phone', 'address', 'date_joined', 'is_active', 'is_staff')
+
+class CustomRegisterSerializer(RegisterSerializer):
+    username = None
+    email = serializers.EmailField(
+        required=True,
+        validators=[UniqueValidator(queryset=User.objects.all())]
+    )
+    name = serializers.CharField(required=True, max_length=255)
+    phone = serializers.CharField(required=False, allow_blank=True, max_length=15)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop('username')
+    
+    def get_cleaned_data(self):
+        data = super().get_cleaned_data()
+        data['email'] = self.validated_data.get('email', '')
+        data['name'] = self.validated_data.get('name', '')
+        data['phone'] = self.validated_data.get('phone', '')
+        return data
+    
+    def save(self, request):
+        user = super().save(request)
+        for fields, value in self.validated_data.items():
+            if hasattr(user, fields):
+                setattr(user, fields, value)
+        user.is_active = True
+        user.save()
+        return user
